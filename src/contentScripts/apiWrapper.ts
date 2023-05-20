b6a3b88fb6558192d2f84240ca0808a4043148c5
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-21 02:57:02 +0900

    Add login

---
+++ src/contentScripts/apiWrapper.ts
@@ -0,0 +1,34 @@
+export const CAMPUSMATE_LOGIN_URL = 'https://portal.nkz.ac.jp/portal/login.do';
+export const CAMPUSMATE_TOP_URL = 'https://portal.nkz.ac.jp/portal/top.do'
+
+export const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+
+    console.log(form!.userId.value, form!.password.value)
+
+    const params = new URLSearchParams();
+    params.append('buttonName', 'login');
+    params.append('userId', form!.userId.value);
+    params.append('password', form!.password.value);
+
+    const request = new Request(CAMPUSMATE_LOGIN_URL, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: new Headers([
+            [ 'Content-Type', 'application/x-www-form-urlencoded' ]
+        ]),
+        body: params,
+    });
+
+    return fetch(request).then((response): boolean => {
+        if (response.redirected) {
+            location.href = CAMPUSMATE_TOP_URL;
+
+            return true;
+        } else {
+            return false;
+        }
+    });
+};
