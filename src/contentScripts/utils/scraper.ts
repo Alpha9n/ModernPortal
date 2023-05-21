9489922c5d57cfca52c60d3e857578b3cee64b3e
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-21 21:01:55 +0900

    chore: missing semi

--- src/contentScripts/utils/scraper.ts
+++ src/contentScripts/utils/scraper.ts
@@ -1,3 +1,3 @@
 const campusmateContainer = document.querySelector<HTMLDivElement>('body > div#container')!;
 
-export const isUserLoggedIn = () => campusmateContainer.querySelector('#loginArea') == null
+export const isUserLoggedIn = () => campusmateContainer.querySelector('#loginArea') == null;
