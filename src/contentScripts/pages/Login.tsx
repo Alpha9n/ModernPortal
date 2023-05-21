959c7c85522630819cc574d0eb00fb37853501f4
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-21 15:48:45 +0900

    ref: Refactor login

--- src/contentScripts/pages/Login.tsx
+++ src/contentScripts/pages/Login.tsx
@@ -1,4 +1,5 @@
-import { handleLogin } from '../apiWrapper';
+import { useRef } from 'react';
+import { login as loginToCampusmate } from '../apiWrapper';
 import { Header } from '../components/Header';
 import { Stack, Center, Input, Text, Button, FormControl } from '@chakra-ui/react';
 interface LoginProps {
@@ -7,6 +8,15 @@ interface LoginProps {
 }
 
 export const Login = ({ title, topPageURL }: LoginProps) => {
+    const userIdRef = useRef<HTMLInputElement>(null!);
+    const passwordRef = useRef<HTMLInputElement>(null!);
+
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+        console.log(e);
+        e.preventDefault();
+        loginToCampusmate(userIdRef.current.value, passwordRef.current.value);
+    }
+
     return (
         <>
             <Header title={title} topPageURL={topPageURL} />
@@ -14,19 +24,21 @@ export const Login = ({ title, topPageURL }: LoginProps) => {
                 m={'2rem auto'}
                 w={'50%'}>
                 <Stack>
-                    <form onSubmit={async (e) => {
-                        await handleLogin(e);
+                    <form onSubmit={(e) => {
+                        handleLogin(e);
                     }}>
                         <FormControl>
                             <Text>ユーザID</Text>
                             <Input type='hidden' name='login' />
                             <Input
+                                ref={userIdRef}
                                 placeholder='User ID'
                                 type='text'
                                 name='userId'
                                 />
                             <Text>パスワード</Text>
                             <Input
+                                ref={passwordRef}
                                 placeholder='Password'
                                 type='password'
                                 name='password'
