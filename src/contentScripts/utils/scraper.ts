a14f3e6746411ed543b19d23c5d977f28582edb2
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-21 20:52:27 +0900

    Add scraper

---
+++ src/contentScripts/utils/scraper.ts
@@ -0,0 +1,3 @@
+const campusmateContainer = document.querySelector<HTMLDivElement>('body > div#container')!;
+
+export const isUserLoggedIn = () => campusmateContainer.querySelector('#loginArea') == null
